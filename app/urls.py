59dcb3ee853bed0f088cdc574e48d66d149c121e
amorfitho <fabian.valentin.ga@gmail.com>
2025-07-04 18:03:46 -0400

    re hacer 2
    cositas

--- app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home,agenda,empleado,login
-
-urlpatterns = [
-    path('',home, name="home"),
-    path('agendar/',agenda, name="agendar"),
-    path('empleado/',empleado, name="trabajador"),
-    path('login/',login,name="login"),
-]
